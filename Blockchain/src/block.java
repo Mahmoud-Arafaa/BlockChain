a38b1d21b021bb54b0815fc5baa86eac998d012d
Mahmoud-Arafaa <45467513+Mahmoud-Arafaa@users.noreply.github.com>
2020-05-07 07:14:42 +0200

    Update block.java

--- Blockchain/src/block.java
+++ Blockchain/src/block.java
@@ -9,8 +9,7 @@ public class block {
         this.data = data;
         this.previousHash = previousHash;
         this.hash  = Arrays.hashCode(new Integer[]{data.hashCode(), previousHash});										 
-        /*function to mix the pre_hash with the hash of the cur block "data" ;	
-         but i have "data" as String so using INTEGER to convert from String to integer and get Arrays.hash to both data-hash and previous hash */													
+													
         														
 }
 	public int getPreviousHash() {
